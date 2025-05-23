81a555a979b77c2119c47042634483016071de62
sdegutis <sbdegutis@gmail.com>
2025-05-22 21:58:29 -0500

    Generate files.

---
+++ main.ts
@@ -0,0 +1,38 @@
+import { transformSync } from '@babel/core'
+import { rmSync } from "fs"
+import { FileTree, generateFiles, Pipeline } from "immaculata"
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+const tree = new FileTree('src', import.meta.url)
+
+const files = Pipeline.from(tree.files)
+
+files.with(/\.tsx?$/).do(file => {
+  file.path = file.path.replace(/\.tsx?$/, '.js')
+  file.text = transformSync(file.text, {
+    plugins: [
+
+      [require('@babel/plugin-transform-typescript'), { isTSX: true }],
+      [require('@babel/plugin-transform-react-jsx'), { runtime: 'automatic' }],
+      {
+        visitor: {
+          ImportDeclaration: {
+            enter: (path) => {
+              const dep = path.node.source?.value
+              if (!dep) return
+
+              if (dep === 'react/jsx-runtime') {
+                path.node.source.value = '/foo/bar.js'
+              }
+            },
+          }
+        }
+      }
+    ],
+  })?.code!
+})
+
+rmSync('docs', { force: true, recursive: true })
+generateFiles(files.results())
--- src/test1.tsx
+++ src/test1.tsx
@@ -1,1 +1,10 @@
 export const foo = 123
+export const qux = <qux />
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [key: string]: { a?: number }
+    }
+  }
+}
