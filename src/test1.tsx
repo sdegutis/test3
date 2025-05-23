e387b44ad0f80e890105f09c09222db463d7b586
sdegutis <sbdegutis@gmail.com>
2025-05-23 01:42:44 -0500

    Test?

--- src/test1.tsx
+++ src/test1.tsx
@@ -1,6 +1,6 @@
 import { foo as foo2 } from 'api.90s.dev/test1.js'
 
-export const foo = foo2
+export const foo = foo2 + 1
 export const qux = <qux a={33} />
 
 export * as bla from 'api.90s.dev/test2.js'
