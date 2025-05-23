84548ac60cb9ce43840521f78829543c31d9d98a
sdegutis <sbdegutis@gmail.com>
2025-05-23 01:22:38 -0500

    Seems fine...

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ import { transform } from 'quickexport'
 
 transform(new FileTree('src', import.meta.url), {
   watch: process.argv[2] === 'dev',
-  jsxImport: 'api.90s.dev/_jsx.js'
+  jsxImport: 'api.90s.dev/_jsx.js',
 })
