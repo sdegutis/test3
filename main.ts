47bd8935bc9ed8a0a47aae175b822b29161ac03d
sdegutis <sbdegutis@gmail.com>
2025-05-23 07:26:37 -0500

    Fix.

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@ import { transform } from 'quickexport'
 
 transform(new FileTree('src', import.meta.url), {
   watch: process.argv[2] === 'dev',
-  jsxImport: '/_jsx2.js',
+  jsxImport: '/test3/_jsx2.js',
 })
