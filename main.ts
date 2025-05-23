522adf960b5ccf2c7eb4f1f652ea831386325f9d
sdegutis <sbdegutis@gmail.com>
2025-05-22 22:14:59 -0500

    Node's --watch is kinda broken.

--- main.ts
+++ main.ts
@@ -1,38 +1,43 @@
 import { transformSync } from '@babel/core'
-import { rmSync } from "fs"
 import { FileTree, generateFiles, Pipeline } from "immaculata"
-import { createRequire } from 'module'
+import { rmSync } from "node:fs"
+import { createRequire } from 'node:module'
 
 const require = createRequire(import.meta.url)
 
 const tree = new FileTree('src', import.meta.url)
 
-const files = Pipeline.from(tree.files)
+transformAll()
+tree.watch().on('filesUpdated', transformAll)
 
-files.with(/\.tsx?$/).do(file => {
-  file.path = file.path.replace(/\.tsx?$/, '.js')
-  file.text = transformSync(file.text, {
-    plugins: [
+function transformAll() {
+  const files = Pipeline.from(tree.files)
 
-      [require('@babel/plugin-transform-typescript'), { isTSX: true }],
-      [require('@babel/plugin-transform-react-jsx'), { runtime: 'automatic' }],
-      {
-        visitor: {
-          ImportDeclaration: {
-            enter: (path) => {
-              const dep = path.node.source?.value
-              if (!dep) return
+  files.with(/\.tsx?$/).do(file => {
+    file.path = file.path.replace(/\.tsx?$/, '.js')
+    file.text = transformSync(file.text, {
+      plugins: [
 
-              if (dep === 'react/jsx-runtime') {
-                path.node.source.value = '/_jsx.js'
-              }
-            },
+        [require('@babel/plugin-transform-typescript'), { isTSX: true }],
+        [require('@babel/plugin-transform-react-jsx'), { runtime: 'automatic' }],
+        {
+          visitor: {
+            ImportDeclaration: {
+              enter: (path) => {
+                const dep = path.node.source?.value
+                if (!dep) return
+
+                if (dep === 'react/jsx-runtime') {
+                  path.node.source.value = '/_jsx.js'
+                }
+              },
+            }
           }
         }
-      }
-    ],
-  })?.code!
-})
+      ],
+    })?.code!
+  })
 
-rmSync('docs', { force: true, recursive: true })
-generateFiles(files.results())
+  rmSync('docs', { force: true, recursive: true })
+  generateFiles(files.results())
+}
