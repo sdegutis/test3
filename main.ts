ad9c6b8dcda91fb484de877118acd3872df36963
sdegutis <sbdegutis@gmail.com>
2025-05-23 09:25:27 -0500

    Use immaculata babel plugin.

--- main.ts
+++ main.ts
@@ -1,27 +1,26 @@
-import { transformSync, type PluginItem } from '@babel/core'
-import { DevServer, FileTree, generateFiles, Pipeline } from "immaculata"
-import { readFileSync, rmSync } from "node:fs"
+import { transformSync } from '@babel/core'
+import { babel, DevServer, FileTree, generateFiles, Pipeline } from "immaculata"
 import { createRequire } from 'node:module'
 
 const isDev = process.argv[2] === 'dev'
 
 const src = new FileTree('src', import.meta.url)
 
-const transform = makeTransform({
-  // 'react/jsx-runtime': 'https://esm.sh/react',
-  'react/jsx-runtime': '/test3/_jsx2.js',
-})
+const transform = makeTransform()
 
 if (isDev) {
   const server = new DevServer(8181, { prefix: '/test3' })
-  transformSrcDir(server)
-  src.watch().on('filesUpdated', () => transformSrcDir(server))
+  server.files = transformSrcDir()
+  src.watch().on('filesUpdated', () => {
+    server.files = transformSrcDir()
+  })
 }
 else {
-  transformSrcDir()
+  const map = transformSrcDir()
+  generateFiles(map)
 }
 
-function transformSrcDir(server?: DevServer) {
+function transformSrcDir() {
   const files = Pipeline.from(src.files)
 
   files.with(/\.tsx?$/).do(file => {
@@ -29,65 +28,19 @@ function transformSrcDir(server?: DevServer) {
     file.text = transform(file.text)
   })
 
-  const map = files.results()
-  if (server) server.files = map
-  rmSync('docs', { force: true, recursive: true })
-  generateFiles(map)
-  return map
+  return files.results()
 }
 
-
-
-function transformImportsPlugin(replacements?: Record<string, string>) {
-
-  const transformImports: PluginItem = {
-    visitor: {
-      ImportDeclaration: {
-        enter(path) {
-          modifyPath(path.node.source)
-        },
-      },
-      ExportDeclaration: {
-        enter(path) {
-          if ('source' in path.node && path.node.source?.value) {
-            modifyPath(path.node.source)
-          }
-        }
-      },
-    }
-  }
-
-  return transformImports
-
-  function modifyPath(source: babel.types.StringLiteral) {
-    const dep = source.value
-    if (dep.match(/^[./]/) || dep.startsWith('http')) return
-
-    if (replacements && dep in replacements) {
-      source.value = replacements[dep]!
-      return
-    }
-
-    const split = dep.indexOf('/')
-    const lib = dep.slice(0, split)
-    const imported = dep.slice(split)
-
-    const pkgjson = JSON.parse(readFileSync('node_modules/' + lib + '/package.json', 'utf8'))
-    const baseurl = new URL(imported, pkgjson.homepage)
-
-    source.value = baseurl.href
-  }
-
-}
-
-
-function makeTransform(replacements?: Record<string, string>) {
+function makeTransform() {
   const require = createRequire(import.meta.url)
   return (text: string) => transformSync(text, {
     plugins: [
       [require('@babel/plugin-transform-typescript'), { isTSX: true }],
       [require('@babel/plugin-transform-react-jsx'), { runtime: 'automatic' }],
-      transformImportsPlugin(replacements)
+      babel.transformImportsPlugin(import.meta.dirname, {
+        'react/jsx-runtime': 'https://esm.sh/react',
+        // 'react/jsx-runtime': '/test3/_jsx2.js',
+      })
     ],
   })!.code!
 }
