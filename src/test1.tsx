a87b3356df0f9bb154268fb8a36ae19f3c3cef99
sdegutis <sbdegutis@gmail.com>
2025-05-23 01:44:11 -0500

    lol oops

--- src/test1.tsx
+++ src/test1.tsx
@@ -4,5 +4,5 @@ export const foo = foo2 + 1
 export const qux = <qux a={33} />
 
 export * as bla from 'api.90s.dev/test2.js'
-export * as bla2 from './test2.js'
+// export * as bla2 from './test2.js'
 
